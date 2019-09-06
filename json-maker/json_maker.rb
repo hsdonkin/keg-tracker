a7a496409f4d3b638dba430da2f84e484045acef
Hugh Sherman  Donkin <hsdonkin@gmail.com>
2019-09-06 09:28:23 -0700

    add more properties to json

--- json-maker/json_maker.rb
+++ json-maker/json_maker.rb
@@ -10,12 +10,17 @@ keglist = {:kegs => []}
   hash = {:name => Faker::Beer.name,
     :brand => Faker::Beer.brand,
     :price => rand(237..296),
-    :abv => rand(3.2..7).round(2)
+    :abv => rand(3.2..7).round(2),
+    :pints_remain => rand(0..124),
+    :ingredients => Faker::Coffee.notes,
+    :description => Faker::Hipster.paragraph
   }
   keglist[:kegs].push(hash)
 end
 
 json = keglist.to_json
+
+Dir.chdir './src/assets'
 
 File.open("kegs-sample.json", "w") do |f|
   f.write(json)
