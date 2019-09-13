bb9153b1e6416b13d22b3ef134f6682c88e3f569
Hugh Sherman  Donkin <hsdonkin@gmail.com>
2019-09-13 09:18:10 -0700

    refactor product list to display app state

--- src/components/ProductListView.jsx
+++ src/components/ProductListView.jsx
@@ -3,23 +3,21 @@ import { Link } from "react-router-dom";
 import kegsData from "./../assets/kegsData.json";
 import ProductList from "./ProductList";
 
-class ProductListView extends Component {
-  render() {
-    const productListItems = kegsData.kegs.map((keg, index) => (
-      <ProductList keg={keg} key={keg.id} />
-    ));
+function ProductListView(props) {
+  const productListItems = props.kegsList.map((keg, index) => (
+    <ProductList keg={keg} key={keg.id} />
+  ));
 
-    return (
-      <React.Fragment>
-        <div className="product-nav">
-          <h2>Products</h2>
-          <Link to="/products/cards">Card View</Link>
-          <Link to="/products/list">List View</Link>
-        </div>
-        <div className="product-list-wrapper">{productListItems}</div>
-      </React.Fragment>
-    );
-  }
+  return (
+    <React.Fragment>
+      <div className="product-nav">
+        <h2>Products</h2>
+        <Link to="/products/cards">Card View</Link>
+        <Link to="/products/list">List View</Link>
+      </div>
+      <div className="product-list-wrapper">{productListItems}</div>
+    </React.Fragment>
+  );
 }
 
 export default ProductListView;
